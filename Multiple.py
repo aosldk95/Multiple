22d6b02c63b557f81e20885c7f29cab7a666d1f3
강병안 <aosldk95@naver.com>
2019-06-20 17:39:02 +0900

    Multiple

--- Multiple.py
+++ Multiple.py
@@ -1,10 +1,13 @@
 number = 1000
 number3=[]
 number5=[]
+number15=[]
 for i in range(0,number,3):
     number3.append(i)
 for i in range(0,number,5):
     number5.append(i)
-print(sum(number3)+sum(number5))
+for i in range(0,number,15):
+    number15.append(i)
+print(sum(number3)+sum(number5)-sum(number15))
 
     
